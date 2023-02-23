64f01c221459312c9c9833d8a50fa2a8c0d0c581
szymonW <szymon.wyderka@gmail.com>
2023-02-23 08:11:40 +0100

    Added main_test.dart file;

--- test/main_test.dart
+++ test/main_test.dart
@@ -9,85 +9,22 @@
 // import 'dart:math';
 //
 // import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:game_queue/db/database.dart';
 
-// import 'package:game_queue/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:game_queue/pages/home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_test/hive_test.dart';
-// import 'package:hive/src/hive_impl.dart';
-// import 'package:path/path.dart' as path;
 
 
 void main() {
+
   setUp(() async {
     await setUpTestHive();
   });
-  //
-  // String tempPath =
-  // path.join(Directory.current.path, '.dart_tool', 'test', 'tmp');
-  //
-  // Future<Directory> getTempDir() async {
-  //   var name = Random().nextInt(pow(2, 32) as int);
-  //   var dir = Directory(path.join(tempPath, '${name}_tmp'));
-  //   if (await dir.exists()) {
-  //     await dir.delete(recursive: true);
-  //   }
-  //   await dir.create(recursive: true);
-  //   return dir;
-  // }
-  //
-  // Future<HiveImpl> initHive() async {
-  //   var tempDir = await getTempDir();
-  //   var hive = HiveImpl();
-  //   hive.init(tempDir.path);
-  //   return hive;
-  // }
 
-  // test('.init()', () {
-  //   var hive = HiveImpl();
-  //
-  //   expect(() => hive.init('MYPATH'), returnsNormally);
-  //   expect(hive.homePath, 'MYPATH');
-  // });
-
-  testWidgets('opened box is returned if it exists', (WidgetTester tester) async {
-    // var hive = await initHive();
-    // await hive.close();
-    // tester.runAsync(() => Hive.openBox('playersBox'));
-    // await tester.pumpWidget(const MyApp());
-    //
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-    //   var hive = HiveImpl();
-    tester.runAsync(() => Hive.openBox('playersBox'));
-    // final _playersBox = Hive.box('playersBox');
-    // PalyersDataBase db = PalyersDataBase();
-    // tester.runAsync(() => hive.openBox('playersBox'));
-    // await tester.pumpWidget(const MyApp());
-
-    // expect(box, true);
-
+  testWidgets('opened box is returned if it exists', (tester) async {
+    await tester.runAsync(() => Hive.openBox('playersBox'));
+      await tester.pumpWidget(const HomePage()).whenComplete(() => Hive.openBox('playersBox'));
     await Hive.close();
   });
-
-  // testWidgets('Adding palyer smoke test', (WidgetTester tester) async {
-  //   // await Hive.initFlutter();
-  //   var hive = await initHive();
-  //   var box = await hive.openBox("playersBox");
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // // Verify that our counter starts at 0.
-  //   // expect(find.text('0'), findsOneWidget);
-  //   // expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   // expect(find.text('0'), findsNothing);
-  //   // expect(find.text('1'), findsOneWidget);
-  // });
 }
