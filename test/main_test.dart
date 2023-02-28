13620beec540f69c0962ed32ac86fbecdabcf469
szymonW <szymon.wyderka@gmail.com>
2023-02-28 13:01:41 +0100

    Create unit tests (#3)
    * Create main_test.dart file; Try to run unit tests;
    
    * Added main_test.dart file;
    
    * Small refactor of tests.

---
+++ test/main_test.dart
@@ -0,0 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+// import 'dart:io';
+// import 'dart:math';
+//
+// import 'package:flutter/material.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:game_queue/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_test/hive_test.dart';
+
+
+void main() {
+
+  setUp(() async {
+    await setUpTestHive();
+  });
+
+  testWidgets('Smoke test - openBox works', (tester) async {
+    await tester.runAsync(() => Hive.openBox('playersBox'));
+      await tester.pumpWidget(const HomePage()).whenComplete(() => Hive.openBox('playersBox'));
+    await Hive.close();
+  });
+}
